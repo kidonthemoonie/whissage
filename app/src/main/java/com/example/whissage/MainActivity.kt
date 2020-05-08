92ac2812c6a6e0c5d85edbcaf646c4b62b10dedc
fvckyounimu <fvckyounimu@gmail.com>
2020-05-08 22:30:20 +0300

    Move from main screen to chat one.

--- app/src/main/java/com/example/whissage/MainActivity.kt
+++ app/src/main/java/com/example/whissage/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.whissage
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val b = findViewById<Button>(R.id.skipButton)
+        b.setOnClickListener{
+            val i = Intent(this, ChatActivity:: class.java)
+            startActivity(i)
+        }
+
     }
 
     override fun onStart() {
